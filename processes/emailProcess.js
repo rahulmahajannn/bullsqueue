25c56695842e2d8d77820924b420cb0bb29b7d2d
rahulmahajannn <rahulmahajan@sprinto.com>
2022-08-22 11:05:51 +0530

    changes done

--- processes/emailProcess.js
+++ processes/emailProcess.js
@@ -1,9 +1,4 @@
 const emailProcess = async (job) => {
-  var delayInMilliseconds = 10000; //1 second
-
-  setTimeout(function () {
-    //your code to be executed after 1 second
-  }, delayInMilliseconds);
   //   throw new Error("something went wrong");
   console.log(job.data);
 };
